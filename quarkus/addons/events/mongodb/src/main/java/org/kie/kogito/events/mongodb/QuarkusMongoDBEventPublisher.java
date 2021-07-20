xiezhang7/submarine-runtimes

KOGITO-3759: Implement event persistence based on MongoDB

addons/events/kogito-events-mongodb-addon/kogito-events-mongodb-quarkus-addon/src/main/java/org/kie/kogito/events/mongodb/QuarkusMongoDBEventPublisher.java
@@ -1,0 +1,115 @@
+/*
+ * Copyright 2021 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kie.kogito.events.mongodb;
+
+import com.mongodb.client.ClientSession;
+import com.mongodb.client.MongoClient;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.kie.kogito.persistence.transaction.TransactionManager;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.util.Optional;
+
+@Singleton
+public class QuarkusMongoDBEventPublisher extends MongoDBEventPublisher {
+
+    @Inject
+    MongoClient mongoClient;
+
+    @Inject
+    TransactionManager<ClientSession> transactionManager;
+
+    @Inject
+    @ConfigProperty(name = "kogito.events.processinstances.enabled")
+    Optional<Boolean> processInstancesEvents;
+
+    @Inject
+    @ConfigProperty(name = "kogito.events.usertasks.enabled")
+    Optional<Boolean> userTasksEvents;
+
+    @Inject
+    @ConfigProperty(name = "kogito.events.variables.enabled")
+    Optional<Boolean> variablesEvents;
+
+    @Inject
+    @ConfigProperty(name = "kogito.events.database", defaultValue = "kogito-events")
+    String eventsDatabaseName;
+
+    @Inject
+    @ConfigProperty(name = "kogito.events.processinstances.collection", defaultValue = "kogitoprocessinstancesevents")
+    String processInstancesEventsCollection;
+
+    @Inject
+    @ConfigProperty(name = "kogito.events.usertasks.collection", defaultValue = "kogitousertaskinstancesevents")
+    String userTasksEventsCollection;
+
+    @Inject
+    @ConfigProperty(name = "kogito.events.variables.collection", defaultValue = "kogitovariablesevents")
+    String variablesEventsCollection;
+
+    @PostConstruct
+    public void setup() {
+        super.configure();
+    }
+
+    @Override
+    protected MongoClient mongoClient() {
+        return this.mongoClient;
+    }
+
+    @Override
+    protected TransactionManager<ClientSession> transactionManager() {
+        return this.transactionManager;
+    }
+
+    @Override
+    protected boolean processInstancesEvents() {
+        return this.processInstancesEvents.orElse(true);
+    }
+
+    @Override
+    protected boolean userTasksEvents() {
+        return this.userTasksEvents.orElse(true);
+    }
+
+    @Override
+    protected boolean variablesEvents() {
+        return this.variablesEvents.orElse(true);
+    }
+
+    @Override
+    protected String eventsDatabaseName() {
+        return this.eventsDatabaseName;
+    }
+
+    @Override
+    protected String processInstancesEventsCollection() {
+        return this.processInstancesEventsCollection;
+    }
+
+    @Override
+    protected String userTasksEventsCollection() {
+        return this.userTasksEventsCollection;
+    }
+
+    @Override
+    protected String variablesEventsCollection() {
+        return this.variablesEventsCollection;
+    }
+}

addons/events/kogito-events-mongodb-addon/kogito-events-mongodb-springboot-addon/src/main/java/org/kie/kogito/events/mongodb/SpringbootMongoDBEventPublisher.java
@@ -1,0 +1,107 @@
+/*
+ * Copyright 2021 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kie.kogito.events.mongodb;
+
+import com.mongodb.client.ClientSession;
+import com.mongodb.client.MongoClient;
+import org.kie.kogito.persistence.transaction.TransactionManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class SpringbootMongoDBEventPublisher extends MongoDBEventPublisher {
+
+    @Autowired
+    MongoClient mongoClient;
+
+    @Autowired
+    TransactionManager<ClientSession> transactionManager;
+
+    @Value("${kogito.events.processinstances.enabled:true}")
+    boolean processInstancesEvents;
+
+    @Value("${kogito.events.usertasks.enabled:true}")
+    boolean userTasksEvents;
+
+    @Value("${kogito.events.variables.enabled:true}")
+    boolean variablesEvents;
+
+    @Value("${kogito.events.database:kogito-events}")
+    String eventsDatabaseName;
+
+    @Value("${kogito.events.processinstances.collection:kogitoprocessinstancesevents}")
+    String processInstancesEventsCollection;
+
+    @Value("${kogito.events.usertasks.collection:kogitousertaskinstancesevents}")
+    String userTasksEventsCollection;
+
+    @Value("${kogito.events.variables.collection:kogitovariablesevents}")
+    String variablesEventsCollection;
+
+    @PostConstruct
+    public void setup() {
+        super.configure();
+    }
+
+    @Override
+    protected MongoClient mongoClient() {
+        return this.mongoClient;
+    }
+
+    @Override
+    protected TransactionManager<ClientSession> transactionManager() {
+        return this.transactionManager;
+    }
+
+    @Override
+    protected boolean processInstancesEvents() {
+        return this.processInstancesEvents;
+    }
+
+    @Override
+    protected boolean userTasksEvents() {
+        return this.userTasksEvents;
+    }
+
+    @Override
+    protected boolean variablesEvents() {
+        return this.variablesEvents;
+    }
+
+    @Override
+    protected String eventsDatabaseName() {
+        return this.eventsDatabaseName;
+    }
+
+    @Override
+    protected String processInstancesEventsCollection() {
+        return this.processInstancesEventsCollection;
+    }
+
+    @Override
+    protected String userTasksEventsCollection() {
+        return this.userTasksEventsCollection;
+    }
+
+    @Override
+    protected String variablesEventsCollection() {
+        return this.variablesEventsCollection;
+    }
+}

xiezhang7/kogito-runtimes

quarkus/addons/events/mongodb/src/main/java/org/kie/kogito/events/mongodb/QuarkusMongoDBEventPublisher.java
@@ -1,0 +1,116 @@
+/*
+ * Copyright 2021 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kie.kogito.events.mongodb;
+
+import java.util.Optional;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.kie.kogito.mongodb.transaction.MongoDBTransactionManager;
+
+import com.mongodb.client.MongoClient;
+
+@Singleton
+public class QuarkusMongoDBEventPublisher extends MongoDBEventPublisher {
+
+    @Inject
+    MongoClient mongoClient;
+
+    @Inject
+    MongoDBTransactionManager transactionManager;
+
+    @Inject
+    @ConfigProperty(name = "kogito.events.processinstances.enabled")
+    Optional<Boolean> processInstancesEvents;
+
+    @Inject
+    @ConfigProperty(name = "kogito.events.usertasks.enabled")
+    Optional<Boolean> userTasksEvents;
+
+    @Inject
+    @ConfigProperty(name = "kogito.events.variables.enabled")
+    Optional<Boolean> variablesEvents;
+
+    @Inject
+    @ConfigProperty(name = "kogito.events.database", defaultValue = "kogito-events")
+    String eventsDatabaseName;
+
+    @Inject
+    @ConfigProperty(name = "kogito.events.processinstances.collection", defaultValue = "kogitoprocessinstancesevents")
+    String processInstancesEventsCollection;
+
+    @Inject
+    @ConfigProperty(name = "kogito.events.usertasks.collection", defaultValue = "kogitousertaskinstancesevents")
+    String userTasksEventsCollection;
+
+    @Inject
+    @ConfigProperty(name = "kogito.events.variables.collection", defaultValue = "kogitovariablesevents")
+    String variablesEventsCollection;
+
+    @PostConstruct
+    public void setup() {
+        super.configure();
+    }
+
+    @Override
+    protected MongoClient mongoClient() {
+        return this.mongoClient;
+    }
+
+    @Override
+    protected MongoDBTransactionManager transactionManager() {
+        return this.transactionManager;
+    }
+
+    @Override
+    protected boolean processInstancesEvents() {
+        return this.processInstancesEvents.orElse(true);
+    }
+
+    @Override
+    protected boolean userTasksEvents() {
+        return this.userTasksEvents.orElse(true);
+    }
+
+    @Override
+    protected boolean variablesEvents() {
+        return this.variablesEvents.orElse(true);
+    }
+
+    @Override
+    protected String eventsDatabaseName() {
+        return this.eventsDatabaseName;
+    }
+
+    @Override
+    protected String processInstancesEventsCollection() {
+        return this.processInstancesEventsCollection;
+    }
+
+    @Override
+    protected String userTasksEventsCollection() {
+        return this.userTasksEventsCollection;
+    }
+
+    @Override
+    protected String variablesEventsCollection() {
+        return this.variablesEventsCollection;
+    }
+}

springboot/addons/events/mongodb/src/main/java/org/kie/kogito/events/mongodb/SpringbootMongoDBEventPublisher.java
@@ -1,0 +1,107 @@
+/*
+ * Copyright 2021 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kie.kogito.events.mongodb;
+
+import javax.annotation.PostConstruct;
+
+import org.kie.kogito.mongodb.transaction.MongoDBTransactionManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.mongodb.client.MongoClient;
+
+@Component
+public class SpringbootMongoDBEventPublisher extends MongoDBEventPublisher {
+
+    @Autowired
+    MongoClient mongoClient;
+
+    @Autowired
+    MongoDBTransactionManager transactionManager;
+
+    @Value("${kogito.events.processinstances.enabled:true}")
+    boolean processInstancesEvents;
+
+    @Value("${kogito.events.usertasks.enabled:true}")
+    boolean userTasksEvents;
+
+    @Value("${kogito.events.variables.enabled:true}")
+    boolean variablesEvents;
+
+    @Value("${kogito.events.database:kogito-events}")
+    String eventsDatabaseName;
+
+    @Value("${kogito.events.processinstances.collection:kogitoprocessinstancesevents}")
+    String processInstancesEventsCollection;
+
+    @Value("${kogito.events.usertasks.collection:kogitousertaskinstancesevents}")
+    String userTasksEventsCollection;
+
+    @Value("${kogito.events.variables.collection:kogitovariablesevents}")
+    String variablesEventsCollection;
+
+    @PostConstruct
+    public void setup() {
+        super.configure();
+    }
+
+    @Override
+    protected MongoClient mongoClient() {
+        return this.mongoClient;
+    }
+
+    @Override
+    protected MongoDBTransactionManager transactionManager() {
+        return this.transactionManager;
+    }
+
+    @Override
+    protected boolean processInstancesEvents() {
+        return this.processInstancesEvents;
+    }
+
+    @Override
+    protected boolean userTasksEvents() {
+        return this.userTasksEvents;
+    }
+
+    @Override
+    protected boolean variablesEvents() {
+        return this.variablesEvents;
+    }
+
+    @Override
+    protected String eventsDatabaseName() {
+        return this.eventsDatabaseName;
+    }
+
+    @Override
+    protected String processInstancesEventsCollection() {
+        return this.processInstancesEventsCollection;
+    }
+
+    @Override
+    protected String userTasksEventsCollection() {
+        return this.userTasksEventsCollection;
+    }
+
+    @Override
+    protected String variablesEventsCollection() {
+        return this.variablesEventsCollection;
+    }
+}